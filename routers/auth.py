6cf23a0f0b09707a1b962f60e2e5ec1e795d9a0d
Rüveyda Durmuş <ruveyda2934@gmail.com>
2025-03-17 22:30:10 +0300

    Refactor import statements and update static file path in FastAPI application

--- routers/auth.py
+++ routers/auth.py
@@ -5,8 +5,8 @@ from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from starlette import status
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-from database import SessionLocal
-from models import User
+from ..database import SessionLocal
+from ..models import User
 import models
 from passlib.context import CryptContext
 from jose import jwt, JWTError
--- routers/todo.py
+++ routers/todo.py
@@ -7,10 +7,10 @@ from sqlalchemy.orm import Session
 from starlette import status
 from starlette.responses import RedirectResponse
 from dotenv import load_dotenv
-from models import Base, Todo
-from database import engine, SessionLocal
+from ..models import Base, Todo
+from ..database import engine, SessionLocal
 from pathlib import Path
-from .auth import get_current_user
+from ..routers.auth import get_current_user
 import google.generativeai as genai
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
@@ -22,7 +22,7 @@ router = APIRouter(
     tags=["Todo"]
 )
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="app/templates")
 
 class TodoRequest(BaseModel):
     title: str = Field(min_length=3)
